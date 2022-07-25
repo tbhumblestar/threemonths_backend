d90fa079c4bd6caaa155dac7a4bbb92fd928cef8
김영빈 <colock123@gmail.com>
2022-07-26 03:03:07 +0900

    feat_add:ProducTListview - dynamic filtering, nested serializer filtering

--- products/serializers.py
+++ products/serializers.py
@@ -8,7 +8,6 @@ class IndependentImageSerializer(serializers.ModelSerializer):
         fields = ['img_src','description']
 
 
-
 #List serializer 데이터 하나하나에 적용됨
 #print("a")를 중간에 끼워넣으면 쿼리셋만큼 a가 출력됨
 class FilteredProductImageSerializer(serializers.ListSerializer):
@@ -18,10 +17,7 @@ class FilteredProductImageSerializer(serializers.ListSerializer):
         #참고) 다음과 같이 상위 시리얼라이저의 context에 접근도 가능하다
         # print("self.parent.context : ",self.parent.context)
         
-        
-        print("child_self.context : ",self.context)
-        img_filter = self.context['img_filter']
-        print(img_filter)
+        img_filter = self.child.context['img_filter']
         if self.context:
             data = data.filter(**img_filter)
         
@@ -43,31 +39,19 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    
-    # main_list_img_src = serializers.SerializerMethodField()
-    # def get_main_list_img_src(self,object):
-    #     return object.productimage_set.get(page='main',place='list').img_src
-    
+        
     productimage_set = ProductImageSerializer(many=True,read_only=True)
     
     def __init__(self,*args,**kwargs):
-        
-        self.additional_context = kwargs.pop('additional_context',{})
-        self.want_fields        = self.additional_context.get('want_fields',{})
-        self.img_filter         = self.additional_context.get('img_filter',{})
-            
         super().__init__(*args,**kwargs)
+        want_fields = self.context.get('want_fields')
         
-        print("self.context : ",self.context)
-        print("self.fields[productimage_set].context : ",self.fields['productimage_set'].context)
-        print("self.fields[productimage_set].context type :",type(self.fields['productimage_set'].context))
-        
-        
-        self.fields['productimage_set'].context.update({"img_filter":self.img_filter})
+        if want_fields:
+            allowed = set(want_fields)
+            existing = set(self.fields)
+            for field_name in existing-allowed:
+                self.fields.pop(field_name)
 
-        print("updated!")
-        
-        print("self.fields[productimage_set].context :",self.fields['productimage_set'].context)
                 
     class Meta:
         model = Product
@@ -77,11 +61,4 @@ class ProductSerializer(serializers.ModelSerializer):
                 'description',
                 'optional_description',
                 'productimage_set',
-                # 'main_list_img_src'
                 ]
-        
-        
-
-#동적 필터링
-#list serializer 커스터마이징(get_initial > to_representation)
-#Serializer Method Field
--- products/views.py
+++ products/views.py
@@ -23,85 +23,33 @@ class ProductListView(generics.ListAPIView):
     filter_backends  = [filters.DjangoFilterBackend]
     filterset_class  = ProductFilter
     
-    # def get_serializer_context(self):
-    #     context = super().get_serializer_context()
-    #     context["test"] = "test"
-    #     # context["query_params"] = self.request.query_params
-    #     return context
-
-    
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
+    def get_serializer_context(self):
+        
         additional_context = {}
 
         #dyanamic field filtering
-        want_fields     = request.query_params.get('fields')
+        want_fields     = self.request.query_params.get('fields')
         if want_fields:
             additional_context['want_fields'] = tuple(want_fields.split(','))
 
-        #img_dict
-        img_dict = request.query_params.get('img_dict')
-        print("here!!!!")
-        print("here!!!!")
-        print(img_dict)
-        print(type(img_dict))
-        if img_dict:
-            print("hi!")
-            print("hi!")
-            splited_img_dict = re.sub(",",":",img_dict).split(":") #자동으로 각 요소들이 str타입으로 리스트에  저장됨
+        #img_filter
+        img_filter = self.request.query_params.get('img_filter')
+        if img_filter:
+            splited_img_filter = re.sub(",",":",img_filter).split(":") #자동으로 각 요소들이 str타입으로 리스트에  저장됨
             
-            # #list to dict 방법1
-            it = iter(re.sub(",",":",img_dict).split(":"))
+            ## list to dict 방법1
+            it = iter(re.sub(",",":",img_filter).split(":"))
             img_filter = dict(zip(it,it))
-            print("img_filter : ",img_filter)
             
-            #list to dict 방법2
-            # lst = splited_img_dict
+            ## list to dict 방법2
+            # lst = splited_img_filter
             # img_filter = {
             #     lst[i] : lst[i+1] for i in range(0,len(lst),2)
             # }
-
             additional_context['img_filter'] = img_filter
         
-        print("here!!!!")
-        print("here!!!!")
-            
-        #만들어진 context_dict를 serializer에 전달
-       
+        #context_update
+        context = super().get_serializer_context()
+        context.update(additional_context)
         
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True,
-                                            additional_context=additional_context)
-
-            return self.get_paginated_response(serializer.data)
-        
-        serializer = self.get_serializer(queryset, many=True,
-                                        additional_context=additional_context)
-        return Response(serializer.data)
-    
-    
-    
-
-    # serializer = self.get_serializer(data=request.data)
-    # serializer.context["customer_id"] = request.user.id
-    # serializer.context["query_params"] = request.query_params
-
-    # serializer.is_valid(raise_exception=True)
-    # ...
-    
-
-    ##이걸 리스트로 바꿔서
-    #리스트에서 리퀘스트쿼리파람스 > dict.update로 context에 넣어줌
-    #listerializer에서 update를 시켜줌
-    
-    
-    # def post(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-
-    #     serializer.context["customer_id"] = request.user.id
-    #     serializer.context["query_params"] = request.query_params
-
-    #     serializer.is_valid(raise_exception=True)
-    #     ...
+        return context
