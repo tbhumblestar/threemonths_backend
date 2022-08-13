aa29fdbf9fb26e2be16aa1a3cba2fffb6b1d0a2e
김영빈 <colock123@gmail.com>
2022-08-13 16:08:22 +0900

    ProductListRetrieveTestCase / add : test_list_product_category_bread

--- products/tests.py
+++ products/tests.py
@@ -134,8 +134,72 @@ class ProductListRetrieveTestCase(APITestCase):
         Product.objects.bulk_create(setup_product_list)
         ProductImage.objects.bulk_create(setup_product_image_list)
         
-    # def test_list_product_category_bread(self):
-    #     pass
+    def test_list_product_category_bread(self):
+        
+        data = [
+            {
+            "id"                   : 1,
+            "category"             : 'bread',
+            "product_name"         : 'test_product1',
+            "price"                : 10000,
+            "optional_description" : 'test_optional_description1',
+            "description"          : 'test_description',
+            "is_active"            : True,
+            "product_images"       : [
+                {
+                'id'          : 1, 
+                'product'     : 1,
+                'img_src'     : 'test_img1@test.com',
+                'page'        : 'main',
+                'place'       : 'list',
+                'description' : 'test_desc1',
+            },
+                {
+                'id'          : 2, 
+                'product'     : 1,
+                'img_src'     : 'test_img2@test.com',
+                'page'        : 'detail',
+                'place'       : 'list',
+                'description' : 'test_desc2',
+            },
+            ],
+        },
+        {
+            "id"                   : 2,
+            "category"             : 'bread',
+            "product_name"         : 'test_product2',
+            "price"                : 10000,
+            "optional_description" : 'test_optional_description2',
+            "description"          : 'test_description',
+            "is_active"            : True,
+            "product_images"       : [
+                {
+                'id'         : 3,
+                'img_src'    : 'test_img3@test.com',
+                'page'       : 'main',
+                'place'      : 'list',
+                'description': 'test_desc3',
+                'product'    : 2
+                },
+                {
+                'id'         : 4,
+                'img_src'    : 'test_img4@test.com',
+                'page'       : 'main',
+                'place'      : 'list',
+                'description': 'test_desc4',
+                'product'    : 2
+                }
+                ],
+        },
+        
+        ]
+        #follow=True옵션을 주면 301에러를 피할 수 있음
+        response = self.client.get('/products?category=bread',follow=True)
+        
+        print(response.json())
+        
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.json(),data)
     
     # def test_list_product_img_filter(self):
     #     pass
@@ -172,10 +236,8 @@ class ProductListRetrieveTestCase(APITestCase):
             ],
         }
     
-        
-        # response , self.client.get(reverse('ProductDetail'),args=(1,))
-        response = self.client.get(reverse('ProductDetail',args=(1,)))
+        response = self.client.get(reverse('ProductDetail',kwargs={'product_id':1}))
         
         self.assertEqual(response.status_code,status.HTTP_200_OK)
-        print(response.json())
         self.assertEqual(response.data,data)
+        
