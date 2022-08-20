6503fb94f95fdbcebead0d8310b02ec7c837a52d
김영빈 <colock123@gmail.com>
2022-08-20 17:07:40 +0900

    QnASerializer / modify : user_nickname field

--- announcements/serializers.py
+++ announcements/serializers.py
@@ -1,24 +1,28 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from announcements.models       import FAQ, QnA, QnAComment
 
 
-class FAQSerializer(ModelSerializer):
+class FAQSerializer(serializers.ModelSerializer):
     
     class Meta:
         model  = FAQ
         fields = ['id','question','answer','created_at']
         
         
-class QnACommentSerializer(ModelSerializer):
+class QnACommentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model  = QnAComment
         fields = ['id','content','created_at']
         
         
-class QnASerializer(ModelSerializer):
+class QnASerializer(serializers.ModelSerializer):
     qna_comments = QnACommentSerializer(many=True,read_only=True)
+    user_nickname = serializers.SerializerMethodField()
     
     class Meta:
         model  = QnA
-        fields = ['id','title','content','qna_comments','created_at']
+        fields = ['id','title','content','qna_comments','created_at','user_nickname']
+        
+    def get_user_nickname(self,object):
+        return object.user.nickname
